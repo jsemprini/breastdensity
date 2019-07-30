af23a6e76a25dc75226f6b6c2c8db7c8f55ee1b4
Jason Semprini <30425529+jsemprini@users.noreply.github.com>
2019-07-29 23:51:47 -0500

    Add files via upload

--- r code/densityplots_rate.R
+++ r code/densityplots_rate.R
@@ -1,48 +1,195 @@
+tiff("plot1.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results1))
+dev.off()
+
+tiff("plot2.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results2))
+dev.off()
+
+tiff("plot3.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results3, na.rm=TRUE))
+dev.off()
+tiff("plot4.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
+
 plot(density(results4, na.rm=TRUE))
+dev.off()
+tiff("plot5.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results5, na.rm=TRUE))
+dev.off()
+tiff("plot6.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results6, na.rm=TRUE))
+dev.off()
+tiff("plot7.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results7))
+dev.off()
+tiff("plot8.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results8))
+dev.off()
+tiff("plot9.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results9, na.rm=TRUE))
+dev.off()
+tiff("plot10.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results10, na.rm=TRUE))
+dev.off()
+tiff("plot11.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results11, na.rm=TRUE))
+dev.off()
+tiff("plot12.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results12, na.rm=TRUE))
+dev.off()
+tiff("plot13.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results13))
+dev.off()
+tiff("plot14.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results14))
+dev.off()
+tiff("plot15.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results15))
+dev.off()
+tiff("plot16.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results16))
+dev.off()
+tiff("plot17.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results17))
+dev.off()
+tiff("plot18.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results18))
+dev.off()
+tiff("plot19.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results19))
+dev.off()
+tiff("plot20.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results20))
+dev.off()
+tiff("plot21.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results21))
+dev.off()
+tiff("plot22.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results22))
+dev.off()
+tiff("plot23.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results23))
+dev.off()
+tiff("plo24.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results24))
+dev.off()
+tiff("plot25.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results25))
+dev.off()
+tiff("plot26.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results26))
+dev.off()
+tiff("plot27.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results27))
+dev.off()
+tiff("plot28.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results28))
+dev.off()
+tiff("plot29.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results29))
+dev.off()
+tiff("plot30.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results30))
+dev.off()
+tiff("plot31.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results31))
+dev.off()
+tiff("plot32.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results32))
+dev.off()
+tiff("plot33.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results33))
+dev.off()
+tiff("plot34.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results34))
+dev.off()
+tiff("plot35.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results35))
+dev.off()
+tiff("plot36.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results36))
+dev.off()
+tiff("plot37.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results37))
+dev.off()
+tiff("plot38.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results38))
+dev.off()
+tiff("plot39.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results39))
+dev.off()
+tiff("plot40.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results40, na.rm=TRUE))
+dev.off()
+tiff("plot41.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results41))
+dev.off()
+tiff("plot42.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results42))
+dev.off()
+tiff("plot43.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results43))
+dev.off()
+tiff("plot44.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results44))
+dev.off()
+tiff("plot45.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results45))
+dev.off()
+tiff("plot46.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results46, na.rm=TRUE))
+dev.off()
+tiff("plot47.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results47))
+dev.off()
+tiff("plot48.tiff", height = 12, width = 17, units = 'cm', 
+     compression = "lzw", res = 1000)
 plot(density(results48))
+dev.off()
